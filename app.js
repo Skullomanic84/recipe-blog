01934373c3ec0275964dc04070de769692df05c4
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2023-10-08 17:36:10 +0200

    updated file uploads and express flash

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const fileUpload = require("express-fileupload");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
 
 
 const app = express();
@@ -14,6 +18,16 @@ app.use(expressLayouts);
 
 
 //layout path
+app.use(cookieParser("CookingBlogSecure"));
+app.use(
+  session({
+    secret: "CookingBlogSecretSession",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+app.use(flash());
+app.use(fileUpload());
 app.set('layout', './layouts/main');
 
 // engine view
