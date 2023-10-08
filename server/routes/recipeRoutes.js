d7607a9b82aca7476107e0f9ddeb35225506507f
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2023-10-08 17:40:06 +0200

    added get submit-recipe and post submit-recipe

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -14,7 +14,8 @@ router.get("/categories/:id", recipeController.exploreCategoriesById);
 router.post('/search', recipeController.searchRecipe);
 router.get("/explore-latest", recipeController.exploreLatest);
 router.get("/explore-random", recipeController.exploreRandom);
-router.get("/submit", recipeController.submitRecipe);
+router.get("/submit-recipe", recipeController.submitRecipe);
+router.post("/submit-recipe", recipeController.submitRecipeOnPost);
 
 
 module.exports= router;
